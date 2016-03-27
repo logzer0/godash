0fd71a70b225e7ca0909291248443359b3696bfb
Aditya Relangi <arelangi@gmail.com>
2016-03-26 23:49:35 -0500

    Add tweetability

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"log"
+	"net/url"
 
+	"github.com/ChimeraCoder/anaconda"
 	"github.com/go-ini/ini"
 )
 
@@ -10,7 +12,7 @@ var Config *ini.File
 
 func main() {
 	// Define buttons and action function
-	DashMacs["74:c2:46:84:f5:ee"] = NoAction
+	DashMacs["74:c2:46:84:f5:ee"] = Tweet
 
 	// Kick it off!
 	SnifferStart()
@@ -18,4 +20,22 @@ func main() {
 
 func NoAction() {
 	log.Println("No action on click.")
+}
+
+func Tweet() {
+	PostTweet("Testing now")
+}
+
+func PostTweet(tweet string) {
+	anaconda.SetConsumerKey(consumerKey)
+	anaconda.SetConsumerSecret(consumerSecret)
+	api := anaconda.NewTwitterApi(accessToken, accessSecret)
+
+	v := url.Values{}
+
+	_, err := api.PostTweet(tweet, v)
+	if err != nil {
+		log.Println("We have an error now ", err)
+	}
+
 }
